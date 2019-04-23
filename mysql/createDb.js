9e3088e61cbc4fdf23300c6bc58ad86c1e212097
Brijesh Kushwaha <brijeshgzp05@gmail.com>
2019-04-23 06:55:16 +0530

    second commit

--- mysql/createDb.js
+++ mysql/createDb.js
@@ -1,14 +1,14 @@
-var mysql = require('mysql');  
-var con = mysql.createConnection({  
-host: "localhost",  
-user: "root",  
-password: "root"  
-});  
-con.connect(function(err) {  
-if (err) throw err;  
-console.log("Connected!");  
-con.query("CREATE DATABASE logindb", function (err, result) {  
-if (err) throw err;  
-console.log("Database created");  
-});  
-});  
+var mysql = require('mysql')
+var con = mysql.createConnection({
+  host: '',
+  user: '',
+  password: ''
+})
+con.connect(function (err) {
+  if (err) throw err
+  console.log('Connected!')
+  con.query('CREATE DATABASE <DB name>', function (err, result) {
+    if (err) throw err
+    console.log('Database created')
+  })
+})
